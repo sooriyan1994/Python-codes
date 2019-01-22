e70981e70acf92d66a1d76cf7de1e1b0f11273ba
sooriyan1994 <45000714+sooriyan1994@users.noreply.github.com>
2019-01-22 18:42:51 +0530

    Add files via upload

--- ex2.py
+++ ex2.py
@@ -1,7 +1,9 @@
 #importing libraries for plotting and matrix operations
 #import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib.pyplot as plt
+import scipy.optimize as op
+#import matplotlib.pyplot as plt
+from logistic_reg_cost_fn import cost_fn, gradient_descent
 
 #Opening the data file and reading data
 with open('ex2data1.txt') as fl:
@@ -13,20 +15,30 @@ with open('ex2data1.txt') as fl:
 V = np.array(V, dtype = float)
 
 #getting X and y from the data
-X = V[:,range(2)].reshape((len(V),1)) #.hstack is to add columns, similarly .vstack for rows
+X = V[:,range(2)].reshape((len(V),2)) #.hstack is to add columns, similarly .vstack for rows
 y = V[:,2].reshape((len(V),1))
 
 #visualising the two features
 pos = np.where(y == 1)
 neg = np.where(y == 0)
-plt.plot(X[pos,0], X[pos,1], 'yo', X[neg,0], X[neg,1], 'rx')
-plt.xlabel('Exam 1 score')
-plt.ylabel('Exam 2 score')
-plt.legend('Admitted', ' Not Admitted')
+#plt.plot(X[pos,0], X[pos,1], 'yo', X[neg,0], X[neg,1], 'rx')
+#plt.xlabel('Exam 1 score')
+#plt.ylabel('Exam 2 score')
+#plt.legend('Admitted', ' Not Admitted')
 
 #adding the bias term to the data
-X = np.vstack(([np.ones(len(V)),X])).transpose()
+X_b = np.hstack((np.ones([len(V),1]),X))
 
 #Initializing theta
-theta = np.zeros([len(V),1],dtype= float)
+init_theta = np.zeros(([len(X_b[2]),1]),dtype = float)
+
+cost = cost_fn(init_theta, X_b, y)
+slope = gradient_descent(init_theta, X_b, y)
+print slope
+
+theta_opt = op.fmin(cost_fn, x0 = init_theta, args = (X_b, y))
+print theta_opt
+#ADVANCED OPTIMISATION TECHNIQUE
+#theta_op = op.minimize(cost_fn, x0 = init_theta, args = (X_b, y))
+print theta_op
 
--- logistic_reg_cost_fn.py
+++ logistic_reg_cost_fn.py
@@ -1,6 +1,23 @@
-def cost_fn(theta, X, y):
-    m = len(y)
-    J = 0;
-    grad = np.zeros(theta.shape)
+from sigmoid import sigmoid
+import numpy as np
 
-    H = sigmoid(
+def cost_fn(theta, X, y):
+
+    m = len(y)
+    J = 0;  #Cost Function      
+    
+    H = sigmoid(X.dot(theta)) #Hypothesis
+    
+    J = sum(-(np.log(H)*(y) + np.log(1-H)*(1-y)))/m
+    print J
+    return float(J)
+
+
+def gradient_descent(theta, X, y):
+
+    H = sigmoid(X.dot(theta))
+    m = len(y)
+    
+    #Gradient Descent
+    slope = np.transpose(X).dot(H - y)/m
+    return slope
