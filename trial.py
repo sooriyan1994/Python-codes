c00df9ccfad586e99b5879d82592bbe6f1a50b54
sooriyan1994 <45000714+sooriyan1994@users.noreply.github.com>
2019-06-07 19:30:49 +0530

    Add files via upload

--- trial.py
+++ trial.py
@@ -1,42 +1,52 @@
 import numpy as np
 import random
-from dist_bw_pts import distance,new_point
+from dist_bw_pts import distance,new_point,border,coord_plot
 import matplotlib.pyplot as plt
 
 ##d = int(raw_input("Enter the diameter of the fibre (in microns): "))
-d = 6
+d = 4
 A_f = np.pi * d**2 /4
-##A = int(raw_input("Area of the square to be filled: "))
-A = 1600
+##a = int(raw_input("Side of the square area to be filled: "))
+a = 10
+A = a**2
+
 ##V_f = float(raw_input("Preferred Volume fraction: "))
 V_f = 0.45
 n = int(V_f * A/A_f)
+print n
+
+border = border(a)
 
 mag = 3*d/2
 coord = [[0,0]]
+
 for i in range(n):
     k = 1
     while (k > 0):
         np = new_point(mag,coord[i])
-        for j in range(i):
-            if distance(np,coord[j]) < mag:
-                continue
-        break                
-       
+        if (-a/2) <= np[0] <= (a/2) and (-a/2) <= np[1] <= (a/2):
+            for l in range(len(border)):
+                if distance(np,border[l]) < mag:
+                    break
+            else:
+                for j in range(i):
+                    print j
+                    if distance(np,coord[j]) < mag:
+                        break
+                else:
+                    break
+        else:
+            continue
+               
     coord.append(np)
     
 print coord
 
-test = coord[random.randint(0,n)]
-dis = []
-for i in range(n):
-    dis = dis + [distance(test,coord[i])]
-print dis
+##test = coord[random.randint(0,n)]
+##dis = []
+##for i in range(n):
+##    dis = dis + [distance(test,coord[i])]
+##print dis
 
-X = []; Y = [];
-for i in range(n):
-    X = X + [coord[i][0]]
-    Y = Y + [coord[i][1]]
-
-plt.plot(X,Y)
-plt.show()
+coord_plot(border)
+coord_plot(coord)
